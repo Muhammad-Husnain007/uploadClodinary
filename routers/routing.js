cc63e955b89891ccf047a46f7fe5b0edef85d92a
Muhammad Husnain <150600466+Muhammad-Husnain007@users.noreply.github.com>
2023-12-06 23:49:39 +0500

    Update routing.js

--- routers/routing.js
+++ routers/routing.js
@@ -9,38 +9,3 @@ module.exports = router;
 
 
 
-//   try {
-    //     const file = req.file;
-    //     console.log(file);
-    //     const result = await cloudinary.uploader.upload(file.path);
-//     console.log(result); 
-
-//     res.send(result); // Send uploaded file information back as response
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).send("Error uploading file");
-//   }
-
-
-
-
-
-    // const cloudinary = require('cloudinary').v2;
-    // const multer = require('multer');
-    
-    // cloudinary.config({ 
-    //   cloud_name: 'drj6zra5d', 
-    //   api_key: '735237695617333', 
-    //   api_secret: 'hSEMn5dn61HZgPT3CWEN_kwr-YE' 
-    // });
-    
-    // const storage = multer.diskStorage({
-    //   destination: function (req, file, cb) {
-    //     cb(null, './upload'); // Define your upload directory here
-    //   },
-    //   filename: function (req, file, cb) {
-    //     cb(null, file.originalname);
-    //   }
-    // });
-    
-    // const upload = multer({ storage: storage });
